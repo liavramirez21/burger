60211a085611828832a59e9d490a5394c5fa60d7
Lia <liavramirez21@gmail.com>
2018-10-29 19:31:47 -0500

    first

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fajita Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('South West  Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cajun Shrimp Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Lime Burger', true);
